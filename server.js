f91014889e27f1d0120d540e09365eee885f10b6
maheshalli28 <maheshalli28@gmail.com>
2025-10-10 22:18:06 +0530

    server files updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferMaxEntries: 0, // Disable mongoose buffering
+  // bufferMaxEntries: 0, // Disable mongoose buffering
   bufferCommands: false, // Disable mongoose buffering
 })
   .then(() => console.log("✅ Connected to MongoDB with optimized settings"))
